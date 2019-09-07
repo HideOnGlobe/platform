2cb7b0a00f2f9bca2c4f4c38be3bec21ed0e4263
Elison <HideobGlobe.gmail.com>
2019-09-07 17:16:49 +0800

    Initial commit

---
+++ center/src/test/java/centerTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @ProjectName: platform
+ * @Package: PACKAGE_NAME
+ * @Description: -测试类模板
+ * @Author: elison
+ * @CreateDate: 2019/9/5 23:22
+ * @UpdateDate: 2019/9/5 23:22
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class centerTest {
+
+//    @Resource
+
+    @Test
+    public void test() {
+        System.out.println("配置需要测试的service");
+    }
+}
